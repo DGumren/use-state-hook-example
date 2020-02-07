0d5b6f2c431642044fdabd46698ccc01e781a635
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-07 10:56:36 -0500

    Example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Counter from "./components/Counter";
+import "./App.css";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Counter />
     </div>
   );
 }
